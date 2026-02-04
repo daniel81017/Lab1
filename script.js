2bddf268e30985df0ca897134a9c858f0b503b7f
daniel81017 <daniel81017@gmail.com>
2026-02-03 21:48:06 -0500

    script link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById("homepagebutton");
 btn.addEventListener("click", () => {
     console.log("Page redirect to GitHub GeoJSON code");
-    window.open("https://www.utoronto.ca");
+    window.open("https://github.com/daniel81017/Lab1/blob/ecb1b6a9a312922b7a34c30258df3ec4fd80e74d/walthamstow.geojson");
 });
 
 
