a0edfc6f531564a8dfa44f155f14d52d7b1a4fd8
daniel81017 <daniel81017@gmail.com>
2026-02-03 21:37:36 -0500

    homepage complete

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("homepagebutton");
+btn.addEventListener("click", () => {
+    console.log("Page redirect to GitHub GeoJSON code");
+    window.open("https://www.utoronto.ca");
+});
+
+
+// newpage = addEventListener('click', () => {
+//     // open("./index.html")
+//     window.location.href = "./page1.html";
+// }
+
+// )
